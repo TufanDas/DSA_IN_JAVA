18cb23c24c8af0be2776096902e3929aebae1b65
Tufan <dtufan38@gmail.com>
2025-07-04 13:35:09 +0530

    find the length of the string

--- Recursion/StringLength.java
+++ Recursion/StringLength.java
@@ -1,4 +1,22 @@
 package Recursion;
-
+/*
+* Write a program to find Length of a String using Recursion.
+* */
 public class StringLength {
+
+    static int findLength(String str) {
+
+        // Base case: if string is empty, its length is 0
+        if (str.length() == 0) {
+            return 0;
+        }
+
+        // Recursive case: remove the first character and count +1
+        return findLength(str.substring(1)) + 1;
+    }
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        System.out.println("The length of the String is : " + findLength(str));
+    }
 }
